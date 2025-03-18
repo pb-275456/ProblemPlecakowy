7e56d96f793d2dc3372adc0016f0e2d6b5efd9e2
Patrycja <patrycjabiskot@gmail.com>
2025-03-18 19:11:23 +0100

    dodanie testu poprawnosci instancji

--- UnitTests/Test1.cs
+++ UnitTests/Test1.cs
@@ -23,7 +23,7 @@ namespace UnitTests
         [TestMethod]
         public void TestNoValidOption()
         {
-            Problem problem = new Problem(10, 1);
+            Problem problem = new Problem(10, 2);
             int capacity = 0;
             int maxValue = 10;
             var validItemsCount = problem.items.Count(i => i.weight <= capacity && i.weight > 0 && i.value <= maxValue && i.value > 0);
@@ -32,11 +32,37 @@ namespace UnitTests
 
             Assert.IsFalse(validItemsCount > 0);
             if (validItemsCount == 0)
-                Assert.AreEqual(result.ids.Count, 0);
+                Assert.AreEqual(0, result.ids.Count);
         }
 
         [TestMethod]
         public void TestCorrectSolution()
+        {
+            Problem problem = new Problem(10, 3);
+            int capacity = 20;
+            Result expectedResult = new Result();
+            expectedResult.ids = [6, 1, 2, 4, 9];
+            expectedResult.value = 32;
+            expectedResult.weight = 14;
+
+            var result = problem.Solve(capacity);
+            expectedResult.ids.Sort();
+            result.ids.Sort();
+
+
+            CollectionAssert.AreEqual(expectedResult.ids, result.ids);
+            Assert.AreEqual(expectedResult.value, result.value);
+            Assert.AreEqual(expectedResult.weight, result.weight);
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+
+        }
+
+        [TestMethod]
+        public void Test2()
         {
 
         }
